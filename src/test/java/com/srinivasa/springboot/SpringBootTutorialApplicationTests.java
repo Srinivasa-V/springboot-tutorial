883552afa7babb9890fc9f2258036a089f21b13f
Srinivasa V <srini.v.telkar@gmail.com>
2024-12-02 18:14:02 +0530

    Initial commit

---
+++ src/test/java/com/srinivasa/springboot/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srinivasa.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
